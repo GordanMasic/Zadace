31cd455b0cc3ecc2b026efa849cfc369c31786f3
GordanMasic <gordan.masic@bootcamp.ba>
2015-05-31 21:03:45 +0200

    HomeworkWeekend2

---
+++ HomeworkWeekend2/src/Movie.java
@@ -0,0 +1,44 @@
+public class Movie {
+
+	private String name;
+	private Integer year;
+	private String director;
+	private String genere;
+	private double imdb;
+	private boolean oscar;
+
+	/**
+	 * Default constructor of class Movie
+	 * 
+	 * @param name
+	 * @param year
+	 * @param director
+	 * @param IMBD
+	 * @param oscar
+	 */
+	public Movie(String name, Integer year, String director, String genere,
+			Double imdb, Boolean oscar) {
+		this.name = name;
+		this.year = year;
+		this.director = director;
+		this.genere = genere;
+		this.imdb = imdb;
+		this.oscar = oscar;
+	}
+
+	/**
+	 * Method for converting value to string
+	 * 
+	 */
+	public String toString() {
+		String s = "";
+		s = s + "Name: " + name + "\n";
+		s = s + "Year: " + year + "\n";
+		s = s + "Director: " + director + "\n";
+		s = s + "Genere: " + genere + "\n";
+		s = s + "IMDB rating: " + imdb + "\n";
+		s = s + "Oscar: " + oscar + "\n";
+		return s;
+	}
+
+}
---
+++ HomeworkWeekend2/src/Task6.java
@@ -0,0 +1,15 @@
+public class Task6 {
+
+	public static void main(String[] args) {
+
+		Movie movie1 = new Movie("Titanic", 1997, "James Cameron",
+				"Drama,Romance", 7.7, true);
+		Movie movie2 = new Movie("Easy rider", 1969, "Dennis Hopper", "Drama ",
+				7.4, false);
+
+		System.out.println(movie1);
+		System.out.println(movie2);
+
+	}
+
+}
