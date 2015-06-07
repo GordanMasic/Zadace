ef3021156fe8d5a38fa6711c0cf9b9f25a4236bb
GordanMasic <gordan.masic@bootcamp.ba>
2015-06-07 18:38:23 +0200

    Zadaca metode i objekti

---
+++ Animal zadaca/Animal.java
@@ -0,0 +1,18 @@
+public class Animal {
+
+	private String sound;
+
+	public Animal(String sound) {
+		super();
+		this.sound = sound;
+	}
+
+	public void setSound(String sound) {
+		this.sound = sound;
+	}
+
+	public void makeSound() {
+		System.out.println(sound);
+	}
+
+}
---
+++ Animal zadaca/Main.java
@@ -0,0 +1,46 @@
+
+public class Main {
+	
+public static void main(String[] args) {
+		
+		Animal dog = new Animal("woof woof");
+		dog.makeSound();
+		
+		Animal fox = new Animal("https://www.youtube.com/watch?v=jofNR_WkoCE");
+		fox.makeSound();
+		
+		Animal duck = new Animal("quack quack");
+		duck.makeSound();
+		
+		Animal cat = new Animal("meow");
+		cat.makeSound();
+		
+		Animal rooster = new Animal("kuukuuriikuuuu");
+		rooster.makeSound();
+		
+		Animal chicken = new Animal("koko koko kokoda");
+		chicken.makeSound();
+		
+		Animal pigeon = new Animal("guu guu");
+		pigeon.makeSound();
+		
+		Animal owl = new Animal("huu huu");
+		owl.makeSound();
+		
+		Animal frog = new Animal("krek krek");
+		frog.makeSound();
+		
+		Animal birdie = new Animal("tweet tweet");
+		birdie.makeSound();
+		
+		Animal cow = new Animal("muuuuuuu");
+		cow.makeSound();
+		
+		Animal donkey = new Animal("iaaaa iaaaa");
+		donkey.makeSound();
+		
+		Animal snake = new Animal("sssssssss");
+		snake.makeSound();
+		
+	}
+}
